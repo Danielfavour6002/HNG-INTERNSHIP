6db5652a176c5ef2b02b0df764eade01bdb3a194
Favour Daniel <feivodanny.06@gmail.com>
2023-09-11 15:53:54 +0100

    an endpoint with 2 query params

--- main.py
+++ main.py
@@ -1,50 +1,29 @@
-from flask import Flask, request, jsonify
-import datetime
-import pytz
-import os
+from flask import Flask, json, request
+import requests
+from datetime import datetime
 
 app = Flask(__name__)
+today = datetime.now()
 
-@app.route('/get_info', methods=['GET'])
-def get_info():
-    # Get query parameters
-    slack_name = request.args.get('slack_name')
-    track = request.args.get('track')
-    return f'slack_name: {slack_name} , track: {track}'
+@app.route('/api', methods=['GET'])
 
-    # Get current UTC time
-    utc_time = datetime.datetime.now(pytz.utc)
+def user_page():
 
-    # Get the current time offset in hours
-    time_offset_hours = utc_time.astimezone(pytz.timezone('UTC+2')).utcoffset().total_seconds() / 3600
+    slack_name = str(request.args.get('slack_name'))  # slack_name=Favour Daniel
+    track = str(request.args.get('track'))  # track=backend
 
-    # Check if the time offset is within +/-2 hours
-    if -2 <= time_offset_hours <= 2:
-        status = 'Success'
-    else:
-        status = 'Time Offset Error'
+    data_set = {"slack_name": f"{slack_name}",
+    "slack_name" : "Favour Daniel",
+    "current_day": today.strftime('%A'),
+    "utc_time": today.strftime('%Y-%m-%dT%H:%M:%SZ'),
+    "track": f"{track}",
+    "github_file" : "https://github.com/Danielfavour6002/HNG-INTERNSHIP/blob/SOURCE/main.py",
+    "github_repo" : "https://github.com/Danielfavour6002/HNG-INTERNSHIP",
+    "status_code": 200}
 
-    # GitHub URLs
-    github_url_file = "https://github.com/your-username/your-repo/blob/main/app.py"
-    github_url_source = "https://github.com/your-username/your-repo"
+    json_dump = json.dumps(data_set)
 
-    # Format the UTC time as "YYYY-MM-DDTHH:MM:SSZ"
-    formatted_utc_time = utc_time.strftime('%Y-%m-%dT%H:%M:%SZ')
-
-    # Return the information in JSON format
-    info = {
-        'Slack_name': slack_name,
-        'Current_day_of_week': utc_time.strftime('%A'),
-        'Current_UTC_time': formatted_utc_time,
-        'Time_Offset_Hours': time_offset_hours,
-        'Track': track,
-        'GitHub_URL_of_file': github_url_file,
-        'GitHub_URL_of_source_code': github_url_source,
-        'Status_Code': status
-    }
-
-    return jsonify(info)
+    return json_dump
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=7070)
